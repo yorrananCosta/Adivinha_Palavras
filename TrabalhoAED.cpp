410b200a413f68e5343657755a2e8b6a61f7edc8
yorranancosta <yorranan.almeida@gmail.com>
2022-10-01 23:06:31 -0300

    V2
    As funções que estavam declaradas acima do main foram realocadas para header.cpp.
    Corrigido bugs no switch case;
    Adiconado função system("clear") no windows é "cls".
    Desenvolvido sistema de verificação de quais caracteres estão na posição correta.
    Excluido arquivos não utilizados.

--- TrabalhoAED.cpp
+++ TrabalhoAED.cpp
@@ -2,41 +2,35 @@
 #include <cstdlib>
 #include <algorithm>
 #include <ctime>
+#include "header.cpp"
 #include "Pilha.h"
 #define MAX 12
 
 using namespace std;
 
-void embaralhar(string palavra1){
-    string embaralhada = palavra1;
-    random_shuffle( embaralhada.begin(), embaralhada.end());
-    cout << "Palavra embaralhada:  " << embaralhada <<endl;
-}
 
-void revelarPalavra(string palavra1, string aux1){
-    for (int i = 0; i < (palavra1.length() - aux1.length()); i++)
-        cout << "_ ";
-    for (int i = aux1.length(); i >= 0; i--)
-        cout << aux1[i] << " ";
-    cout <<endl;
-}
-
-void mostrarMenuEscolha(){
-    cout << "-----------------------|L E T R O C A|-----------------------"<<endl;
-    cout <<endl << "[1] - Chutar palavra" <<endl << "[2] - Reembaralhar palavra" <<endl << "[3] - Revelar letra" <<endl << "[4] - Sair" <<endl <<endl;
-}
 
 int main(){
-    cout << "Digite a palavra secreta: ";
+    
     string palavra;
-    char letra, letraAnt[MAX];
+    string chute;
+    string aux; 
+    string aux2;
+    char letraAnt[MAX];
+    int letra;
+    int pontos = 0;
+
+    cout << "Digite a palavra secreta: ";
     cin >> palavra;
-    string chute, aux = palavra, aux2;
+    aux = palavra;
+    system("clear");
+    
     no *pilha;
     pilha = inicializaPilha(pilha);
     
-    for (int i = 0; i < aux.length(); i++)
+    for (int i = 0; i < aux.length(); i++) {
         pilha = PUSH(pilha, palavra[i]);
+    }
 
     int choice, conta4 = 0;
     //esconder palavra digitada;
@@ -46,41 +40,58 @@ int main(){
     cin >> choice;
     do{
         switch(choice){
-            case 1:
-            cout << "Chute: " <<endl;
-            cin >> chute;
-            if (chute == palavra){
-                cout << "PARABENS, VOCE ACERTOU!" <<endl;
-                exit(0);
-                break;
+            case 1:{
+                cout << "Chute: " <<endl;
+                cin >> chute;
+                int count = 0;
+                for (int i = 0; i < chute.length(); i++)
+                {
+                    if(comparar_palavras(chute[i], palavra[i])){
+                        cout << "A " << i+1 << "(a) letra esta na posicao certa" << endl;
+                        count++;
+                    }
+                }
+                if(count == palavra.length()){
+                    cout << "PARABENS, VOCE ACERTOU!" <<endl;
+                    exit(0);
+                    break;
+                }
+                else {
+                    cout << "PALAVRA INCORRETA!"<<endl;
+                }
             }
-            else
-                cout << "PALAVRA INCORRETA!"<<endl;
-            
             break;
 
-            case 2:
-            cout << "REEMBARALHANDO..."<<endl;
-            embaralhar(palavra);
+            case 2: {
+                cout << "REEMBARALHANDO..."<<endl;
+                embaralhar(palavra);
+            }
             break;
 
-            case 3:
-            cout <<endl;
-            pilha = POP(pilha, &letra);
-            cout << "LETRA: " << letra <<endl;
-            letraAnt[conta4] = letra;
-            aux2.push_back(letraAnt[conta4]);
-            conta4++;
-            revelarPalavra(palavra, aux2);
-            cout <<endl;
+            case 3: {
+                cout <<endl;
+                pilha = POP(pilha, &letra);
+                cout << "LETRA: [" << char(letra) << "]" << endl;
+                letraAnt[conta4] = letra;
+                aux2.push_back(letraAnt[conta4]);
+                conta4++;
+                revelarPalavra(palavra, aux2);
+                cout <<endl;
+            }
             break;
             
-            case 4:
-            exit(0);
-            break;
+            case 4: {
+                exit(0);
+            }break;
+
+            default: {
+                cout << "ERRO - selecao invalida!" << endl;
+            }
 
         }
         mostrarMenuEscolha();
         cin >> choice;
+        
+        
     }while(choice != 4);
 }
--- header.cpp
+++ header.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <stack>
-#include <stdio.h>
+#include <algorithm>
 
 using namespace std;
-
-void mensage() {
-    cout << "ADVINHADOR DE PALAVRAS" << endl;
-}
 
 string word_receiver(){
     string word_typed;
@@ -14,11 +10,30 @@ string word_receiver(){
     return word_typed;
 }
 
-bool compare_words(char char_compared, char comparator_char) {
-    if(char_compared == comparator_char) {
+bool comparar_palavras(char char_comparada, char char_comparadora) {
+    if(char_comparada == char_comparadora) {
         return true;
     }
     else {
         return false;
     }
+}
+
+void embaralhar(string palavra1){
+    string embaralhada = palavra1;
+    random_shuffle( embaralhada.begin(), embaralhada.end());
+    cout << "Palavra embaralhada:  " << embaralhada <<endl;
+}
+
+void revelarPalavra(string palavra1, string aux1){
+    for (int i = 0; i < (palavra1.length() - aux1.length()); i++)
+        cout << "_ ";
+    for (int i = aux1.length(); i >= 0; i--)
+        cout << aux1[i] << " ";
+    cout <<endl;
+}
+
+void mostrarMenuEscolha(){
+    cout << "-----------------------|L E T R O C A|-----------------------"<<endl;
+    cout <<endl << "[1] - Chutar palavra" <<endl << "[2] - Reembaralhar palavra" <<endl << "[3] - Revelar letra" <<endl << "[4] - Sair" <<endl <<endl;
 }
